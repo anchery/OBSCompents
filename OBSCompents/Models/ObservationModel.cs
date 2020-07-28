ca340cd5e1d6c67eeed26e24498273b9b846d00f
anchery <35939871+anchery@users.noreply.github.com>
2020-07-28 12:49:46 -0500

    Add project files.

---
+++ OBSCompents/Models/ObservationModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OBSCompents.Models
+{
+    public class ObservationGroupModel
+    {
+        public int GrpId { get; set; }
+        public string GrpText { get; set; }
+    }
+    public class ObservationModel
+    {
+        public int ObsId { get; set; }
+        public string ObsText { get; set; }
+    }
+    public class ObservationViewModel
+    {
+        public int GrpId { get; set; }
+        public string GrpText { get; set; }
+        public List<ObservationModel> observations { get; set; }
+        public List<ResponseTypeModel> responseTypes { get; set; }
+    }
+    public class ResponseModel
+    {
+        public int GrpId { get; set; }
+        public int ObsId { get; set; }
+        public int Response { get; set; }
+        public string Comment { get; set; }
+    }
+    public class ResponseTypeModel
+    {
+        public int ResponseId { get; set; }
+        public string ResponseText { get; set; }
+    }
+}
